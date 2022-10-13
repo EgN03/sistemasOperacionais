ca292b3e7367015e1ad7d5e19c95e036c3dc5a71
enzoDamatoDev <enzodamato@outlook.com>
2022-10-13 01:34:55 -0300

    criando a main

--- GrupoEEG/Lab04/pth_mult_matriz.c
+++ GrupoEEG/Lab04/pth_mult_matriz.c
@@ -11,17 +11,57 @@ Executando o programa:
 #include <stdlib.h>
 #include <pthread.h>
 
-void *Pht_mat_vect(void* rank){
-	long my_rank = (long) rank;
-	int i, j;
-	int local_m =  m/thread_count;
-	int my_first_row = my_rank*local_m
-	int my_last_row = (my_rank+1)*local_m -1;
-	
-	for(i = my_first_row; i <= my_last_row; i++){
-		y[i] = 0.0;
-		for(j = 0; j<n; j++)
-			y[i] += A[i][j]*x[j];
+#define M 3  //numero de linha de A e Y
+#define N 2  // numero de colunas de B e Y
+#define K 4  // numero de colunas de A e linhas de B
+#define thread_count 3 //numero de threads
+
+int A[M][K] ={{4,6,2,8}, {7,4,2,7}, {9,0,2,6}};
+int B[K][N] ={{8,2}, {6,6}, {1,0}, {9,3}};
+int C[M][N];
+int y[M];
+
+void imprimeMatriz(int linhas, int colunas, int matriz[linhas][colunas]){ //funcao para imprimir as matrizes
+	int lin,col;
+	for(lin=0;lin<linhas;lin++){
+		for(col=0;col<colunas;col++){
+			printf("%d ", matriz[lin][col]);
+		}
+		printf("\n");
 	}
-	return NULL
+}
+
+void *Pht_mat_vect(void* rank){ // funcao para calcular cada linha da matriz C
+	long my_rank = (long) rank; // liga uma linha da matriz C a thread atual
+	int i, j;
+
+	for(i = 0; i < N; i++){ // roda para todas as colunas de B
+		for(j = 0; j<K; j++) // roda para todas as linhas de B
+			C[my_rank][i] += A[my_rank][j]*B[j][i]; // calcula cada valor de sua linha
+	}
+	return NULL;
 } /*Pth_mat_vect */
+
+
+int main(void){
+	long thread;
+	pthread_t threads[thread_count]; // separa a memoria para as threads
+
+	printf("vetor A:\n");
+	imprimeMatriz(M,K,A);
+	
+	printf("\nvetor B:\n");
+	imprimeMatriz(K,N,B);
+	
+	for(thread =0; thread<thread_count; thread++){
+		pthread_create(&threads[thread], NULL, Pht_mat_vect, (void*) thread); // inicializa cada thread e passa a linha ligada a ela
+	}
+	
+	for(thread =0; thread<thread_count; thread++){
+		pthread_join(threads[thread], NULL); // encerra as threads
+	}
+	
+	printf("\nvetor C:\n");
+	imprimeMatriz(M,N,C);
+	
+}

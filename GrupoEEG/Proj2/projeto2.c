8d2d47c4c56fe838971fad596a32217809976bba
enzoDamatoDev <79601694+enzoDamatoDev@users.noreply.github.com>
2022-11-14 03:12:48 -0300

    Update projeto2.c

--- GrupoEEG/Proj2/projeto2.c
+++ GrupoEEG/Proj2/projeto2.c
@@ -1,8 +1,8 @@
-/*Projeto 2 - Transferência Bancária 
+/*Projeto 2 - TransferÃªncia BancÃ¡ria 
 Integrantes: 
 Enrique Granado Novaes 32107803 
 Enzo Rocha Damato 32125992
-Gustavo Saad Andrade Maluhy*/
+Gustavo Saad Andrade Maluhy 32173091*/
 
 #include <stdio.h>  
 #include <stdlib.h>
@@ -21,20 +21,20 @@ void *transferencia(void *i)//codigo que sera execultado por cada thread
 	int conta = rand()%2 +1;//decide qual conta ira transferir
 	int valor = rand()%100 +1;//decide o valor da transferencia
 	int *from,*to;
-	if(conta == 1){//conta a trnsferir é c1
+	if(conta == 1){//conta a trnsferir Ã© c1
 		from = &c1;
 		to = &c2;
 		printf("  conta c1 transfere %d para conta c2\n", valor);
-	}else{//conta a trnsferir é c2
+	}else{//conta a trnsferir Ã© c2
 		from = &c2;
 		to = &c1;
 		printf("  conta c2 transfere %d para conta c1\n", valor);
 	}
-	if (*from >= valor){ // verifica se há saldo e faz a tranferencia
+	if (*from >= valor){ // verifica se hÃ¡ saldo e faz a tranferencia
 		*from -= valor;
 		*to += valor;
 	}else{
-		printf("Transferência negada!\n");
+		printf("TransferÃªncia negada!\n");
 	}
 	printf("  Saldo de c1: %d\n", c1);
 	printf("  Saldo de c2: %d\n", c2);
@@ -47,7 +47,7 @@ int main(int argc, void* args[])
 	int i ,t=THREADS;//numero de threads a serem criadas
 	if(argc>1){//verifica se foi especificado um numero de threads
 		t = atoi(args[1]);
-		if(t>THREADS){//verifica se é um numero valido
+		if(t>THREADS){//verifica se Ã© um numero valido
 			t=THREADS;
 		}
 	}
